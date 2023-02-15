1f393ca5cc1b5a393b0be5385def563a07fee270
muayad98 <moaaayd1223@gmail.com>
2023-02-15 17:53:29 -0600

    Add sum_list.py file

---
+++ Desktop/spring23/CS3203 SE/sum_list.py
@@ -0,0 +1,10 @@
+def sum_list(numbers):
+    sum = 0
+    for num in numbers:
+        sum += num
+    return sum
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+result = sum_list(numbers)
+print(result)  # Output: 15
