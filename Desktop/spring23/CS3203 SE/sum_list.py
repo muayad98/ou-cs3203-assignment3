d225d823354c0ab59ef965c02636dad081d2f103
muayad98 <moaaayd1223@gmail.com>
2023-02-15 18:09:56 -0600

    Add multiply_list function

--- Desktop/spring23/CS3203 SE/sum_list.py
+++ Desktop/spring23/CS3203 SE/sum_list.py
@@ -4,7 +4,15 @@ def sum_list(numbers):
         sum += num
     return sum
 
+def multiply_list(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
 # Example usage
 numbers = [1, 2, 3, 4, 5]
-result = sum_list(numbers)
-print(result)  # Output: 15
+sum_result = sum_list(numbers)
+multiply_result = multiply_list(numbers)
+print("Sum:", sum_result)  # Output: 15
+print("Product:", multiply_result)  # Output: 120
